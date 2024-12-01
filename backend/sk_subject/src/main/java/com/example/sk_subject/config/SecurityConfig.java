ce9720ac50bb6dd1534fe604e6805b653087a3e3
song-yeonghyun <dudgus907@naver.com>
2024-12-01 18:01:00 +0900

    refactor: 게시글 조회 인증없이 가능하게 변경

--- backend/sk_subject/src/main/java/com/example/sk_subject/config/SecurityConfig.java
+++ backend/sk_subject/src/main/java/com/example/sk_subject/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.sk_subject.config;
 import com.example.sk_subject.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,7 +27,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frame -> frame.disable())) // H2 Console의 iframe 허용
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/h2-console/**", "file/**").permitAll()
+                        // 인증 없이 접근 가능한 URL 패턴 설정
+                        .requestMatchers(HttpMethod.GET, "/auth/login", "/h2-console/**", "/file/**", "/board/all", "/board/{id:[0-9]+}").permitAll()
+                        // 인증이 필요한 URL 패턴 설정 (POST, PUT, DELETE)
+                        .requestMatchers(HttpMethod.POST, "/board/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/board/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/board/**").authenticated()
+                        // 기타 모든 요청은 인증 필요
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
