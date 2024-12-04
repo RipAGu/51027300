ee4f58bfd249f52dd883a49b88c4e40be9f0bfe6
song-yeonghyun <dudgus907@naver.com>
2024-12-04 11:45:29 +0900

    feat: 게시글 수정 기능 추가

--- frontend/sk_subject/src/router/index.js
+++ frontend/sk_subject/src/router/index.js
@@ -3,6 +3,7 @@ import LoginPage from "../views/LoginPage.vue"; // 로그인 페이지 컴포넌
 import BoardPage from "@/views/BoardPage.vue";
 import BoardDetailPage from "@/views/BoardDetailPage.vue";
 import BoardCreatePage from "@/views/BoardCreatePage.vue";
+import BoardEditPage from "@/views/BoardEditPage.vue";
 
 const routes = [
     { path: "/login", name: "LoginPage", component: LoginPage },
@@ -17,6 +18,12 @@ const routes = [
         path: "/board/create",
         name: "BoardCreatePage",
         component: BoardCreatePage,
+    },
+    {
+        path: "/edit/:id",
+        name: "BoardCreatePage",
+        component: BoardEditPage,
+        props: true,
     },
     { path: "/", redirect: "/login" }, // 기본 경로를 /login으로 리디렉션
 ];
